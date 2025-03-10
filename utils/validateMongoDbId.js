50670f9b6b4c98871f6ab66e4b62bb7a549c4529
Amit kumar singh <amitksingh.1204@gmail.com>
2025-03-11 00:13:04 +0530

    userctrl changes

--- utils/validateMongoDbId.js
+++ utils/validateMongoDbId.js
@@ -3,4 +3,4 @@ const validateMongodbId = (id) => {
   const isValid = mongoose.Types.ObjectId.isValid(id);
   if (!isValid) throw new Error("This id is not valid or not Found");
 };
-module.exports = {validateMongodbId}
+module.exports = {validateMongodbId};
